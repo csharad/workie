4439f04c13cc05ad5517e1f1f09fa155de53c8fd
Sharad Chand <sharad.d.chand@gmail.com>
2018-11-12 17:55:24 +0530

    Hide login and signup routes if logged in

--- vue/src/router.js
+++ vue/src/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import store from './store';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -27,3 +28,15 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach(async (to, from, next) => {
+  await store.dispatch('getMe');
+  if ((to.name === 'login' || to.name === 'sign-up') && store.state.me) {
+    // Hide login / sign-up routes if already logged in.
+    next({ name: 'home' });
+  } else {
+    next();
+  }
+});
+
+export default router;
