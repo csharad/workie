61cd44ae43491aee87d122193fa371bfc507e1f5
Sharad Chand <sharad.d.chand@gmail.com>
2018-11-15 18:49:38 +0530

    Seperate the tasks based on their completeness

--- reactjs/src/components/TaskList.js
+++ reactjs/src/components/TaskList.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import { List, ListItem, ListItemText, Typography } from '@material-ui/core';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  withStyles
+} from '@material-ui/core';
 import TaskListItem from './TaskListItem';
 import { connect } from 'react-redux';
 import { listAllTasks } from '../actions';
+
+const styles = theme => ({
+  completed: {
+    padding: `0 ${theme.spacing.unit * 2}px`
+  }
+});
 
 class TaskList extends Component {
   async componentDidMount() {
@@ -10,7 +22,7 @@ class TaskList extends Component {
   }
 
   render() {
-    const { tasks } = this.props;
+    const { tasks, classes } = this.props;
 
     let noTask = null;
     if (tasks.length === 0) {
@@ -25,17 +37,48 @@ class TaskList extends Component {
       );
     }
 
+    const taskItem = task => <TaskListItem key={task.id} task={task} />;
+    const completedTasks = this.completedTasks();
+    let completedLabel = null;
+    if (completedTasks.length !== 0) {
+      completedLabel = (
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          className={classes.completed}
+        >
+          COMPLETED
+        </Typography>
+      );
+    }
+
     return (
       <List>
-        {tasks.map(task => (
-          <TaskListItem key={task.id} task={task} />
-        ))}
+        {this.incompleteTasks().map(taskItem)}
+        {completedLabel}
+        {completedTasks.map(taskItem)}
         {noTask}
       </List>
     );
   }
+
+  incompleteTasks = () => {
+    return this.props.tasks
+      .filter(task => !task.is_completed)
+      .sort((a, b) => (a.id < b.id ? 1 : a.id > b.id ? -1 : 0));
+  };
+
+  completedTasks = () => {
+    return this.props.tasks
+      .filter(task => !!task.is_completed)
+      .sort((a, b) => {
+        const aDate = new Date(a.completed_on).getTime();
+        const bDate = new Date(b.completed_on).getTime();
+        return aDate < bDate ? 1 : aDate > bDate ? -1 : 0;
+      });
+  };
 }
 
 export default connect(state => ({
   tasks: state.tasks
-}))(TaskList);
+}))(withStyles(styles)(TaskList));
